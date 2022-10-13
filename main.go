ca29dbdc4b289859e2ab96b8b25393626ab9b628
mudcrab88 <mudcrab@yandex.ru>
2022-10-14 00:08:01 +0400

    third commit

--- main.go
+++ main.go
@@ -2,28 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"strings"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		io.WriteString(os.Stderr, "No arguments!\n")
-		os.Exit(1)
-	}
-	str := strings.TrimSpace(os.Args[1])
-	space := 32
 	count := 0
-	prev := int(str[0])
+	if len(os.Args) == 2 {
+		str := strings.TrimSpace(os.Args[1])
+		space := 32
 
-	if len(str) > 0 {
-		count = 1
-		for _, val := range str {
-			if int(val) == space && prev != space {
-				count += 1
+		prev := int(str[0])
+
+		if len(str) > 0 {
+			count = 1
+			for _, val := range str {
+				if int(val) == space && prev != space {
+					count += 1
+				}
+				prev = int(val)
 			}
-			prev = int(val)
 		}
 	}
 
